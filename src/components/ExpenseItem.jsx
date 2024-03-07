644cea52d4c5ed71d63f79f3e4c9bd2078a7f36b
alokkumarsingh07 <kalok8841@gmail.com>
2024-03-07 14:51:16 +0530

    expense

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ExpenseItem() {
+  return (
+    <>
+      <h1>Expense Item</h1>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movie Rs 100</p>
+    </>
+  );
+}
+
+export default ExpenseItem;
