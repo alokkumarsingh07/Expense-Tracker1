f7e7bbf537c1779e8d13ba11856f5ca174c86cb9
alokkumarsingh07 <kalok8841@gmail.com>
2024-03-10 02:24:15 +0530

    expense

--- src/components/Expense/ExpenseDetails.jsx
+++ src/components/Expense/ExpenseDetails.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItems.css";
 
-function ExpenseDetails({ title, amt }) {
+function ExpenseDetails(props) {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amt);
+
+  const changeAmt = () => {
+    setAmount("$100");
+    console.log(`${amount}`);
+  };
+
   const clickHandler = () => {
     alert("Clicked!!!");
+    setTitle("Updated!");
+    console.log(title);
   };
   return (
     <>
       <div className="expense-item_description">
         <h2>{title}</h2>
-        <div className="expense-item_price">{amt}</div>
+        <div className="expense-item_price">{amount}</div>
       </div>
       <hr />
       <button className="button" onClick={clickHandler}>
         Clicked-me
+      </button>
+      <br />
+      <button className="button" onClick={changeAmt}>
+        Change-amt
       </button>
     </>
   );
