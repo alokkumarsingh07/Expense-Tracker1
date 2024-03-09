86293b38549b19b6c0db32d0898a180b691c28cf
alokkumarsingh07 <kalok8841@gmail.com>
2024-03-09 13:54:44 +0530

    expense

--- src/components/Expense/ExpenseDate.jsx
+++ src/components/Expense/ExpenseDate.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ExpenseDate.css";
 
 function ExpenseDate({ date }) {
   const month = date.toLocaleString("en-US", { month: "long" });
--- src/components/Expense/ExpenseDetails.jsx
+++ src/components/Expense/ExpenseDetails.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import './ExpenseItems.css'
-function ExpenseDetails({title,amt}) {
+import "./ExpenseItems.css";
+
+function ExpenseDetails({ title, amt }) {
+  const clickHandler = () => {
+    alert("Clicked!!!");
+  };
   return (
     <>
       <div className="expense-item_description">
         <h2>{title}</h2>
         <div className="expense-item_price">{amt}</div>
       </div>
+      <hr />
+      <button className="button" onClick={clickHandler}>
+        Clicked-me
+      </button>
     </>
   );
 }
