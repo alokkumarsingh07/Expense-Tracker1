5f9a540746547c43113c90b266be7a48e0fb2d63
alokkumarsingh07 <kalok8841@gmail.com>
2024-03-10 18:48:37 +0530

    expense

--- src/components/Expense/Expense.jsx
+++ src/components/Expense/Expense.jsx
@@ -4,7 +4,7 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList";
-import ExpensesChart from "./ExpensesChart";
+import ExpensesChart from "./ExpensesChart.jsx";
 import ExpenseItems from "./ExpenseItems";
 
 function Expenses(props) {
